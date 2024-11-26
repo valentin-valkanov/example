6e6e6f4bdecb4b35dbed60a2dd63235c54d2fa65
Valentin Valkanov <valentinvalkanof@gmail.com>
2024-11-26 14:57:26 +0200

    feature/09_episode

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,40 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => "Director",
-                'salary' => '50 000'
-            ],
-            [
-                'id' => 2,
-                'title' => "Programmer",
-                'salary' => '10 000'
-            ],
-            [
-                'id' => 3,
-                'title' => "Teacher",
-                'salary' => '40 000'
-            ],
-        ];
+    protected $table = 'jobs_listings';
 
-    }
+    protected $fillable = ['title', 'salary'];
 
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        if(! $job) {
-            abort(404);
-        }
-
-        return $job;
-    }
 }
