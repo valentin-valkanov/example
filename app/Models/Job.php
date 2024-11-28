dc7018d7086b6da4f5c410f057bcbf0787d0f608
Valentin Valkanov <valentinvalkanof@gmail.com>
2024-11-28 16:47:32 +0200

    feature/12_episode

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -19,4 +19,9 @@ class Job extends Model
         return $this->belongsTo(Employer::class);
     }
 
+    public function tags()
+{
+    return $this->belongsToMany(Tag::class, foreignPivotKey: 'job_listings_id');
+}
+
 }
