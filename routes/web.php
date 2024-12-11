908ae4e68ba2e8d8bba0d79f71c71b5b78adb932
Valentin Valkanov <valentinvalkanof@gmail.com>
2024-12-11 13:03:36 +0200

    feature/24_episode

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
+
+
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
